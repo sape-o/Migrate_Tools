493c0e15feb3abdc216d3eddb3450470248c79eb
โอ้ เองครับ <13391356+sape-o@users.noreply.github.com>
2025-07-01 10:20:29 +0700

    nav finish

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -17,25 +17,34 @@
 const navItems = [
   { name: "List Object", to: "/list-object", icon: "fas fa-home" },
   { name: "List Object Group", to: "/list-object-group", icon: "fas fa-user" },
+  { name: "Import Object", to: "/import-object", icon: "fas fa-home" },
+  { name: "List Policy", to: "/list-policy", icon: "fas fa-user" },
 ];
 </script>
 
 <style scoped>
 .sidebar {
-  width: 200px;
-  height: 100vh;
+  position: fixed; /* ติดกับขอบซ้ายตลอด */
+  top: 20px;
+  left: 10px;
+  height: calc(100vh - 40px); /* เว้นขอบบน-ล่างรวม 40px */  width: 220px;
   background-color: #2c3e50;
+  padding: 15px 10px; /* margin ด้านในเล็กน้อย */
+  border-radius: 12px;   /* มนทั้งกล่อง */
+  box-shadow: 2px 0 8px rgba(0, 0, 0, 0.2);
   display: flex;
   flex-direction: column;
-  padding-top: 20px;
+    margin-left: 10px;     /* เลื่อนออกจากขอบจอเล็กน้อย */
 }
 
 .nav-item {
   color: #ecf0f1;
   text-decoration: none;
-  padding: 15px 20px;
+  padding: 12px 15px;
   display: flex;
   align-items: center;
+  border-radius: 8px;
+  margin-bottom: 8px;
   transition: background 0.3s;
 }
 
@@ -44,7 +53,7 @@ const navItems = [
 }
 
 .nav-item:hover {
-  background-color: #34495e;
+  background-color: #f4ffb6;
 }
 
 .active {
